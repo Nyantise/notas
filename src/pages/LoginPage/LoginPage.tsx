2dc2511c84eadf707e0377482aca003ec3f71c0b
gaesyeah <gabrielgoesrios@gmail.com>
2023-11-30 20:29:35 -0300

    hotfix

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { StyledPage } from "../style";
 import { LoginContainer } from "./style";
 import { useLocation, useNavigate } from "react-router-dom";
